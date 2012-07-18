07403ae4dd25f0ffac2add9300135dc5167024e5
Thomas Kelder <thomas@thomas-laptop.(none)>
2012-07-18 15:06:04 +0200

    - updated gitignore to ignore generated R package file - fix package build script

--- build.package.R
+++ build.package.R
@@ -1,4 +1,6 @@
 #package.skeleton("GSCFClient", code_files = "dbnp.functions.R")
+dir.create("GSCFClient/R")
+dir.create("GSCFClient/man")
 file.copy("dbnp.functions.R", "GSCFClient/R/dbnp.functions.R", overwrite = T)
 package.skeleton.dx("GSCFClient")
 system("R CMD build GSCFClient")
