dc91ec07a59874d05e3b5aa9e9282df867566bbf
Thomas Kelder <thomas@thomas-laptop.(none)>
2012-07-18 15:02:22 +0200

    Created R package

---
+++ build.package.R
@@ -0,0 +1,8 @@
+#package.skeleton("GSCFClient", code_files = "dbnp.functions.R")
+file.copy("dbnp.functions.R", "GSCFClient/R/dbnp.functions.R", overwrite = T)
+package.skeleton.dx("GSCFClient")
+system("R CMD build GSCFClient")
+
+install.packages("GSCFClient_1.0.tar.gz", repo=NULL)
+detach("package:GSCFClient", unload = T)
+library(GSCFClient)
