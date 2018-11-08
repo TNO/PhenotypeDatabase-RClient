33b185c769e25566ec13dbda4649dce2635404b5
Ferry Jagers <ferry.programming@icloud.com>
2018-11-08 15:36:52 +0100

    Adjusted RClient to API adjustments

--- R/phenotype-database.functions.R
+++ R/phenotype-database.functions.R
@@ -40,32 +40,32 @@ library(digest)
 .set("verbose", F)
 
 .getValidation = function() {
-	.set('authSequence', .get('authSequence') + 1)
-	str = paste(.get('authToken'), .get('authSequence'), .get('authKey'), sep="")
+  .set('authSequence', .get('authSequence') + 1)
+  str = paste(.get('authToken'), .get('authSequence'), .get('authKey'), sep="")
   if(.get("verbose")) {
-	  message("Sequence: ", .get('authSequence'))
-	  message("Validation string: ", str)
+    message("Sequence: ", .get('authSequence'))
+    message("Validation string: ", str)
   }
-	digest(str, algo = "md5", serialize = F)
+  digest(str, algo = "md5", serialize = F)
 }
 
 .createUrl = function(base, cmd, pars = list()) {
-	url = paste(base, cmd, "?", sep="")
-	for(p in names(pars)) {
-		v = pars[[p]]
-		if(is.null(v) || v != "") url = paste(url, p, "=", v, "&", sep="")
-	}
-	gsub("[\\?&]{1}$", "", url)
+  url = paste(base, cmd, "?", sep="")
+  for(p in names(pars)) {
+    v = pars[[p]]
+    if(is.null(v) || v != "") url = paste(url, p, "=", v, "&", sep="")
+  }
+  gsub("[\\?&]{1}$", "", url)
 }
 
 .getUrlErr = function(...) {
-	if(.get("verbose")) print(url)
-	err= ""
-	r = getURL(..., verbose = .get("verbose"))
-	if(!isValidJSON(r, asText = T)) {
-		stop(r)
-	}
-	r
+  if(.get("verbose")) print(url)
+  err= ""
+  r = getURL(..., verbose = .get("verbose"))
+  if(!isValidJSON(r, asText = T)) {
+    stop(r)
+  }
+  r
 }
 
 setPhenotypeDatabaseBaseUrl = function(baseUrl = "https://dashin.eu/interventionstudies/api/") {
@@ -89,41 +89,41 @@ authenticate = function
 (user, ##<< The username
  pass, ##<< The password
  shared.key ##<< The shared key (can be found at your profile page in the Phenotype Database web interface)
- ) {
- 	devId = .genDeviceID(user)
-	url = .createUrl(getPhenotypeDatabaseBaseUrl(), "authenticate", list(deviceID = devId))
-	resp = .getUrlErr(url, .opts = curlOptions(userpwd = paste(user, pass, sep=":")))
-	auth = fromJSON(resp)
+) {
+  devId = .genDeviceID(user)
+  url = .createUrl(getPhenotypeDatabaseBaseUrl(), "authenticate", list(deviceID = devId))
+  resp = .getUrlErr(url, .opts = curlOptions(userpwd = paste(user, pass, sep=":")))
+  auth = fromJSON(resp)
   .set("authSequence", auth$sequence)
-	.set("authKey", shared.key)
-	.set("authToken", auth$token)
-	.set("deviceID", devId)
-	T
+  .set("authKey", shared.key)
+  .set("authToken", auth$token)
+  .set("deviceID", devId)
+  T
 }
 
 .fieldAsName = function(x, f = "token") {
-	names(x) = as.character(sapply(x, function(y) y[f]))
-	x
+  names(x) = as.character(sapply(x, function(y) y[f]))
+  x
 }
 
 getStudies = function
 ### Get a list of the available studies
 () {
-	url = .createUrl(getPhenotypeDatabaseBaseUrl(), "getStudies", list(validation = .getValidation(), deviceID = .get("deviceID")))
-	resp = .getUrlErr(url)
-	r = fromJSON(resp)
-	.fieldAsName(r$studies)
+  url = .createUrl(getPhenotypeDatabaseBaseUrl(), "getStudies", list(validation = .getValidation(), deviceID = .get("deviceID")))
+  resp = .getUrlErr(url)
+  r = fromJSON(resp)
+  .fieldAsName(r$studies)
   ### A named list with the available studies
 }
 
 getSubjectsForStudy = function
 ### Get subjects for a given study
 (studyToken ##<< The token of the study to get the subjects for
- ) {
-	url = .createUrl(getPhenotypeDatabaseBaseUrl(), "getSubjectsForStudy", list(studyToken = studyToken, deviceID = .get("deviceID"), validation = .getValidation()))
-	resp = .getUrlErr(url)
-	r = fromJSON(resp)
-	.fieldAsName(r$subjects)
+) {
+  url = .createUrl(getPhenotypeDatabaseBaseUrl(), "getSubjectsForStudy", list(studyToken = studyToken, deviceID = .get("deviceID"), validation = .getValidation()))
+  resp = .getUrlErr(url)
+  r = fromJSON(resp)
+  .fieldAsName(r$subjects)
   ### A named list with the subjects
 }
 
@@ -141,44 +141,44 @@ getSubjectGroupsForStudy = function
 getSampleAndTreatmentGroupsForStudy = function
 ### Get sample & treatment groups for a given study
 (studyToken ##<< The token of the study to get the event groups for
- ) {
-	url = .createUrl(getPhenotypeDatabaseBaseUrl(), "getSampleAndTreatmentGroupsForStudy", list(studyToken = studyToken, deviceID = .get("deviceID"), validation = .getValidation()))
-	resp = .getUrlErr(url)
-	r = fromJSON(resp)
-	.fieldAsName(r$sampleAndTreatmentGroups)
+) {
+  url = .createUrl(getPhenotypeDatabaseBaseUrl(), "getSampleAndTreatmentGroupsForStudy", list(studyToken = studyToken, deviceID = .get("deviceID"), validation = .getValidation()))
+  resp = .getUrlErr(url)
+  r = fromJSON(resp)
+  .fieldAsName(r$sampleAndTreatmentGroups)
   ### A named list with the sample & treatment groups
 }
 
 getTreatmentTypesForStudy = function
 ### Get treatment types for a given study
 (studyToken ##<< The token of the study to get the events for
- ) {
-	url = .createUrl(getPhenotypeDatabaseBaseUrl(), "getTreatmentTypesForStudy", list(studyToken = studyToken, deviceID = .get("deviceID"), validation = .getValidation()))
-	resp = .getUrlErr(url)
-	r = fromJSON(resp)
-	.fieldAsName(r$treatmentTypes)
+) {
+  url = .createUrl(getPhenotypeDatabaseBaseUrl(), "getTreatmentTypesForStudy", list(studyToken = studyToken, deviceID = .get("deviceID"), validation = .getValidation()))
+  resp = .getUrlErr(url)
+  r = fromJSON(resp)
+  .fieldAsName(r$treatmentTypes)
   ### A named list with the treatment types
 }
 
 getSampleTypesForStudy = function
 ### Get sample types for a given study
 (studyToken ##<< The token of the study to get the sampling events for
- ) {
-	url = .createUrl(getPhenotypeDatabaseBaseUrl(), "getSampleTypesForStudy", list(studyToken = studyToken, deviceID = .get("deviceID"), validation = .getValidation()))
-	resp = .getUrlErr(url)
-	r = fromJSON(resp)
-	.fieldAsName(r$sampleTypes)
+) {
+  url = .createUrl(getPhenotypeDatabaseBaseUrl(), "getSampleTypesForStudy", list(studyToken = studyToken, deviceID = .get("deviceID"), validation = .getValidation()))
+  resp = .getUrlErr(url)
+  r = fromJSON(resp)
+  .fieldAsName(r$sampleTypes)
   ### A named list with the sample types
 }
 
 getAssaysForStudy = function
 ### Get the available assays for a given study
 (studyToken ##<< The token of the study to get the assays for
- ) {
-	url = .createUrl(getPhenotypeDatabaseBaseUrl(), "getAssaysForStudy", list(studyToken = studyToken, deviceID = .get("deviceID"), validation = .getValidation()))
-	resp = .getUrlErr(url)
-	r	= fromJSON(resp)
-	.fieldAsName(r$assays)
+) {
+  url = .createUrl(getPhenotypeDatabaseBaseUrl(), "getAssaysForStudy", list(studyToken = studyToken, deviceID = .get("deviceID"), validation = .getValidation()))
+  resp = .getUrlErr(url)
+  r	= fromJSON(resp)
+  .fieldAsName(r$assays)
   ### A named list of the assays
 }
 
@@ -207,11 +207,11 @@ getSubjectsForAssay = function
 getSamplesForAssay = function
 ### Get samples for a given assay
 (assayToken ##<< The token of the assay to get the samples for
- ) {
-	url = .createUrl(getPhenotypeDatabaseBaseUrl(), "getSamplesForAssay", list(assayToken = assayToken, deviceID = .get("deviceID"), validation = .getValidation()))
-	resp = .getUrlErr(url)
-	r = fromJSON(resp)
-	.fieldAsName(r$samples)
+) {
+  url = .createUrl(getPhenotypeDatabaseBaseUrl(), "getSamplesForAssay", list(assayToken = assayToken, deviceID = .get("deviceID"), validation = .getValidation()))
+  resp = .getUrlErr(url)
+  r = fromJSON(resp)
+  .fieldAsName(r$samples)
   ### A named list of the available samples
 }
 
@@ -222,7 +222,7 @@ getFeaturesForAssay = function
   url = .createUrl(getPhenotypeDatabaseBaseUrl(), "getFeaturesForAssay", list(assayToken = assayToken, deviceID = .get("deviceID"), validation = .getValidation()))
   resp = .getUrlErr(url)
   r = fromJSON(resp)
-  .fieldAsName(r$features)
+  r$features
 }
 
 getMeasurementDataForAssay = function
@@ -232,15 +232,229 @@ getMeasurementDataForAssay = function
   url = .createUrl(getPhenotypeDatabaseBaseUrl(), "getMeasurementDataForAssay", list(assayToken = assayToken, deviceID = .get("deviceID"), validation = .getValidation()))
   resp = .getUrlErr(url)
   r = fromJSON(resp)
-  .fieldAsName(r$measurements)
+  r
 }
 
-exportAssayToOpal = function
-### Get data for a given assay
-(assayToken ##<< The token of the assay to get the data for
-) {
-  url = .createUrl(getPhenotypeDatabaseBaseUrl(), "exportAssayToOpal", list(assayToken = assayToken, deviceID = .get("deviceID"), validation = .getValidation()))
-  resp = .getUrlErr(url)
-  r = fromJSON(resp)
-  r
+
+getMoltenAssayList <- function(study_token){
+  parsePhenoDbTime <- function(x){
+    
+    ptime  <- list(w=0,d=0,h=0,m=0,s=0)
+    
+    val    <- unlist(strsplit(x,split = '[a-z]'))
+    
+    header <- unlist(strsplit(x,split = '[0-9]'))
+    header <- header[!header %in% c('','-')]
+    
+    val        <- as.numeric(val)
+    names(val) <- header
+    val        <- as.list(val)
+    
+    for(dtype in names(val) )
+      ptime[[dtype]] <- val[[dtype]]
+    
+    unlist(ptime)
+    
+  }
+  
+  sanitize_str <- function(str){
+    str                  <- gsub("\\s*\\([^\\)]+\\)","",str)
+    str                  <- gsub("[[:punct:]]", "", str)
+    str                  <- gsub("[[:space:]]", "", str)
+    
+    str
+  }
+  
+  assays       <- getAssaysForStudy(study_token)
+  assay_tokens <- lapply(assays, function(x) x[['token']])# tokens for all assays
+  
+  
+  sampleData    <- getSubjectsForStudy(study_token)
+  subject_data  <- do.call(rbind,lapply(sampleData,unlist ))
+  
+  experiments  <- list()
+  sample_maps  <- list()    
+  
+  molten_list <-  lapply(assay_tokens, function(token){ 
+    assay_name =   sanitize_str( assays[[token]][['name']] )
+    
+    assayFeatures = getFeaturesForAssay(token)
+    assayData     = getMeasurementDataForAssay(token)
+    
+    features_measurements = do.call(cbind, lapply(assayData$measurements, function(x){
+      names(x) <- gsub('[.]','',names(x))
+      unlist(x)
+    } ))
+    
+    if(length(features_measurements) == 0 ){
+      warning(paste0('Empty assay for token ',token,' name ',assay_name))
+      return(NULL)
+    }
+    
+    # map sample ids                                              
+    mapped_sample_list <- lapply( strsplit(rownames(features_measurements),split = '[.]'),function(x){
+      type <- x[1]
+      t1   <- x[2]   
+      t2   <- x[3]
+      subj <- x[4]
+      
+      tvec <- parsePhenoDbTime(t1) + parsePhenoDbTime(t2)
+      tstr <- paste0(paste0(tvec,names(tvec)),collapse = '')
+      
+      c(subj,tstr)
+    })
+    
+    mapped_samples           <- do.call(rbind,mapped_sample_list)
+    colnames(mapped_samples) <- c('name','strtime')
+    mapped_samples           <- as.data.frame(mapped_samples)
+    
+    
+    features_measurements_molten       <- reshape2::melt(data = data.frame(features_measurements,mapped_samples,stringsAsFactors = F),
+                                                         id.var=c('name','strtime'))
+    
+    features_measurements_molten$assay <- assay_name
+    features_measurements_molten       <- features_measurements_molten[,c('assay','name','strtime','variable','value')]
+  })
+  # cleanup list names
+  names(molten_list) <- paste0('token_',gsub('-','_',names(molten_list)))
+  
+  return(molten_list)
+}
+
+mapAssayMeasurement <- function(molten_assay_list,token,variable,group_map){
+  
+  molten_assay_list = paris_molten_assay_list[[token]]
+  
+  row_select        = molten_assay_list$variable == variable & paste(molten_assay_list$strtime) %in% names(group_map)
+  molten_assay_list = molten_assay_list[row_select,]
+  
+  time_mapper <- function(x){
+    group_map[[x]]
+  }
+  
+  molten_assay_list$study_time <- sapply(molten_assay_list$strtime, time_mapper)
+  molten_assay_list
+}                         
+
+
+
+getMultiAssayExp <- function(study_token){
+  require(MultiAssayExperiment)
+  require(reshape2)
+  
+  
+  parsePhenoDbTime <- function(x){
+    
+    ptime  <- list(w=0,d=0,h=0,m=0,s=0)
+    
+    val    <- unlist(strsplit(x,split = '[a-z]'))
+    
+    header <- unlist(strsplit(x,split = '[0-9]'))
+    header <- header[!header %in% c('','-')]
+    
+    val        <- as.numeric(val)
+    names(val) <- header
+    val        <- as.list(val)
+    
+    for(dtype in names(val) )
+      ptime[[dtype]] <- val[[dtype]]
+    
+    unlist(ptime)
+    
+  }
+  
+  sanitize_str <- function(str){
+    str                  <- gsub("\\s*\\([^\\)]+\\)","",str)
+    str                  <- gsub("[[:punct:]]", "", str)
+    str                  <- gsub("[[:space:]]", "", str)
+    
+    str
+  }
+  
+  assays       <- getAssaysForStudy(study_token)
+  assay_tokens <- lapply(assays, function(x) x[['token']])# tokens for all assays
+  
+  sampleData    <- getSubjectsForStudy(study_token)
+  subject_data  <- do.call(rbind,lapply(sampleData,unlist ))
+  
+  experiments  <- list()
+  sample_maps  <- list()
+  
+  
+  for(token in assay_tokens){
+    
+    assayFeatures = getFeaturesForAssay(token)
+    assayData     = getMeasurementDataForAssay(token)
+    
+    features_measurements = do.call(cbind, lapply(assayData$measurements, function(x){
+      names(x) <- gsub('[.]','',names(x))
+      unlist(x)
+    } ))
+    
+    if(length(features_measurements) == 0 ){
+      warning(paste0('Empty assay for token ',token,' name ',assay_name))
+      next
+    }
+    
+    # map sample ids                                              
+    mapped_sample_list <- lapply( strsplit(rownames(features_measurements),split = '[.]'),function(x){
+      type <- x[1]
+      t1   <- x[2]   
+      t2   <- x[3]
+      subj <- x[4]
+      
+      tvec <- parsePhenoDbTime(t1) + parsePhenoDbTime(t2)
+      tstr <- paste0(paste0(tvec,names(tvec)),collapse = '')
+      
+      c(subj,tstr)
+    })
+    
+    mapped_samples           <- do.call(rbind,mapped_sample_list)
+    colnames(mapped_samples) <- c('name','strtime')
+    mapped_samples           <- as.data.frame(mapped_samples)
+    
+    
+    features_measurements_molten <- reshape2::melt(data = data.frame(features_measurements,mapped_samples,stringsAsFactors = F),
+                                                   id.var=c('name','strtime'))
+    
+    feature_data                 <- reshape2::dcast(features_measurements_molten, fill = NA,drop = F,
+                                                    value.var = 'value',
+                                                    formula = name+strtime~variable)
+    
+    primary_key               <- paste0(feature_data$name,'_',feature_data$strtime)         
+    colname_key               <- primary_key    
+    
+    rownames(feature_data)    <- primary_key
+    feature_data$name         <- NULL  
+    feature_data$strtime      <- NULL  
+    
+    
+    # add times
+    # add treatment/group information    
+    assay_sample_map      = data.frame(primary = as.character(primary_key),
+                                       colname = as.character(colname_key) )
+    
+    # retrive and sanitize assay name
+    assay_name                  <- assays[[token]][['name']]
+    assay_name                  <- sanitize_str(assay_name)
+    
+    sample_maps[[ assay_name ]] <-  assay_sample_map
+    experiments[[ assay_name ]] <-  t(feature_data)
+  }
+  
+  col_data           <- do.call(rbind,lapply(sample_maps,function(x) x))
+  col_data           <- col_data[!duplicated(col_data$primary),]
+  col_data$name      <- unlist(lapply(strsplit(paste(col_data$primary),split = '_'),function(x) x[[1]] ))
+  col_data           <- merge(col_data,subject_data,by='name')
+  rownames(col_data) <- col_data$primary
+  col_data$str_time  <- unlist(lapply(strsplit(paste(col_data$primary),split = '_'),function(x)x[[2]]))
+  col_data$colname   <- NULL
+  
+  multi_sample_map <- listToMap(sample_maps)                                 
+  
+  multi_assay_exp  <- MultiAssayExperiment(experiments = experiments,
+                                           colData   = col_data,
+                                           sampleMap = multi_sample_map)
+  return(multi_assay_exp)
+  
 }
--- example.R
+++ example.R
@@ -1,69 +1,71 @@
-###############################################
-## Example on Phenotype Database R functions ##
-###############################################
-library(devtools)
-install_github("TNO/PhenotypeDatabase-RClient")
-library(PhenotypeDatabaseRClient)
+#function to check if a list entry is empty
+is.NullOb <- function(x) if(!(is.function(x))) is.null(x) | all(sapply(x, is.null)) else FALSE
 
-## Specify the Phenotype Database instance
+##function to recursively step down into list, removing all empty objects 
+rmNullObs <- function(x) {
+  if(!(is.function(x))) {
+    x = x[!(sapply(x, is.NullOb))]
+    lapply(x, function(x) if (is.list(x)) rmNullObs(x) else x)
+  }
+}
+
+#Uncomment lines below if you want to install PhenotypeDatabaseRClient via Github
+#library(devtools)
+#install_github("TNO/PhenotypeDatabase-RClient")
+
+#load dependencies
+require(PhenotypeDatabaseRClient)
+require(reshape2)
+require(dplyr)
+
+#authentication procedure
 setPhenotypeDatabaseBaseUrl("https://dashin.eu/interventionstudies/api/")
+user = "<username>"
+skey = "<apikey>"
+authenticate(user, paste(readline()), skey)
+#break here to enter password in console
+#authenticate(user, winDialogString("Password:", ""), skey)
 
-## Login to the Phenotype Database instance
-user = "yourUsername"
-
-## Set your password here or type it in console by uncommenting the second line below
-pass = "yourPass"
-#pass = readline("Enter your password in console: ")
-
-## Set your API key (found in your Phenotype Database user profile)
-skey = "yourSharedKey"
-
-authenticate(user, pass, skey)
-
-## Get available studies
+#get available studies & select specific study
 studies = getStudies()
-
-## Look for the Diclofenac study
-study = studies[[grep("Diclofenac", sapply(studies, function(x) x$title))]]
+study = studies[[grep("<studyName>", sapply(studies, function(x) x$code))]]
 studyToken = study['token']
 
-## Get some assay data for the study
+## Get assays for the selected study
 assays = getAssaysForStudy(studyToken)
+assayTokens = names(assays)
 assayNames = sapply(assays, function(x) x$name)
-assayToken = names(assayNames[grep("GCMS", assayNames)])
 
-assaySubjects = getSubjectsForAssay(assayToken)
-assayFeatures = getFeaturesForAssay(assayToken)
-assayData = getMeasurementDataForAssay(assayToken)
+#get subjects in study
+studySubjects = getSubjectsForStudy(studyToken)
+studySubjects = do.call(cbind, studySubjects)
 
-print(paste("Fetched a total of", assayData[['count']], "measurements for the assay."))
+#get features in study and subjects in each assay
+studyFeatures = list()
+studySubjectsPerAssay = list()
+for (i in seq_along(assayTokens)){
+  assayToken = assayTokens[i]
+  studyFeatures[[i]] = getFeaturesForAssay(assayToken)
+  studySubjectsPerAssay[[i]] = getSubjectsForAssay(assayToken)
+}
+studyFeaturesWithData = sapply(studyFeatures, function(x) length(x) > 0)
+studyFeatures = rmNullObs(studyFeatures)
+studyFeaturesLong = melt(studyFeatures)
+colnames(studyFeaturesLong) = c("Value", "Property", "Feature", "Assay")
+studyFeaturesLong$Assay = assayNames[studyFeaturesWithData][studyFeaturesLong$Assay]
+studyFeaturesWide = dcast(data = studyFeaturesLong, formula = Assay + Feature ~ Property, value.var = "Value")
 
-## Get all available timepoints for Inositol
-inositolTimepoints = names(assayData[['measurements']][['Inositol']])
+names(studySubjectsPerAssay) = assayNames
+studySubjectsPerAssay = rmNullObs(studySubjectsPerAssay)
 
-## View all available timepoints for Inositol
-print(inositolTimepoints)
+#get all study data
+studyData = list()
+for (i in seq_along(assayTokens)){
+  assayToken = assayTokens[i]
+  studyData[[i]] = getMeasurementDataForAssay(assayToken)$measurements
+}
+names(studyData) = assayNames
 
-## Get measurments for timepoint 0
-inositolBaseline = assayData[['measurements']][['Inositol']][['0s']]
-## Get measurments for timepoint 1 week 2 days 3 hours
-inositolLast = assayData[['measurements']][['Inositol']][['1w2d3h']]
-
-## Create data.frame
-assayDataFrame <- data.frame(
-  subject = c(names(inositolBaseline),names(inositolLast)),
-  timepoint = rep(c("Baseline", "Last"), each = 19),
-  value = c(inositolBaseline, inositolLast)
-)
-
-## View data frame
-print(assayDataFrame)
-
-## Plot data frame (using ggplot2)
-library(ggplot2)
-p = ggplot(assayDataFrame, aes(x=subject, y=value, fill=timepoint)) +
-  geom_bar(stat="identity", position=position_dodge())
-p
-
-## This requires an Opal (obiba.org/pages/products/opal) instance on the same server as your PhenotypeDatabase instance and some additional Phenotype Database configuration
-exportAssayToOpal(assayToken)
+#studyData = melt(studyData[sapply(studyData, function(x) x$count > 0)])
+studyData = melt(studyData)
+colnames(studyData) = c("Value", "Subject", "SampleType", "SubjectGroup", "RelTime", "AbsTime", "EventGroup", "Variable", "Assay")
