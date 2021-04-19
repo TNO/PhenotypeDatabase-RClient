30f3062a51889b58ac708014374ab85033bd75bb
Serdar Özsezen <serdar.ozsezen@tno.nl>
2021-04-19 14:29:53 +0200

    Change RCurl for httr library

--- R/phenotype-database.functions.R
+++ R/phenotype-database.functions.R
@@ -12,10 +12,14 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-library(RJSONIO)
-library(rjson)
-library(RCurl)
-library(digest)
+#' phenotype-database-functions
+#' @name phenotype-database-functions
+#' @export
+
+require(RJSONIO)
+require(rjson)
+require(httr)
+require(digest)
 
 .vars = new.env()
 
@@ -59,9 +63,8 @@ library(digest)
 }
 
 .getUrlErr = function(...) {
-  if(.get("verbose")) print(url)
-  err= ""
-  r = getURL(..., verbose = .get("verbose"))
+  c = GET(...)
+  r = rawToChar(c$content)
   if(!isValidJSON(r, asText = T)) {
     stop(r)
   }
@@ -84,7 +87,7 @@ setPhenotypeDatabaseVerbose = function(verbose = F) {
   .set("verbose", verbose)
 }
 
-authenticate = function
+doPhenotypeDatabaseAuth = function
 ### Authenticate with the Phenotype Database
 (user, ##<< The username
  pass, ##<< The password
@@ -92,7 +95,8 @@ authenticate = function
 ) {
   devId = .genDeviceID(user)
   url = .createUrl(getPhenotypeDatabaseBaseUrl(), "authenticate", list(deviceID = devId))
-  resp = .getUrlErr(url, .opts = curlOptions(userpwd = paste(user, pass, sep=":")))
+  conf = authenticate(user, pass)
+  resp = .getUrlErr(url, config = conf)
   auth = fromJSON(resp)
   .set("authSequence", auth$sequence)
   .set("authKey", shared.key)
@@ -234,227 +238,64 @@ getMeasurementDataForAssay = function
   r = fromJSON(resp)
   r
 }
-
-
-getMoltenAssayList <- function(study_token){
-  parsePhenoDbTime <- function(x){
-    
-    ptime  <- list(w=0,d=0,h=0,m=0,s=0)
-    
-    val    <- unlist(strsplit(x,split = '[a-z]'))
-    
-    header <- unlist(strsplit(x,split = '[0-9]'))
-    header <- header[!header %in% c('','-')]
-    
-    val        <- as.numeric(val)
-    names(val) <- header
-    val        <- as.list(val)
-    
-    for(dtype in names(val) )
-      ptime[[dtype]] <- val[[dtype]]
-    
-    unlist(ptime)
-    
-  }
-  
-  sanitize_str <- function(str){
-    str                  <- gsub("\\s*\\([^\\)]+\\)","",str)
-    str                  <- gsub("[[:punct:]]", "", str)
-    str                  <- gsub("[[:space:]]", "", str)
-    
-    str
-  }
-  
-  assays       <- getAssaysForStudy(study_token)
-  assay_tokens <- lapply(assays, function(x) x[['token']])# tokens for all assays
-  
-  
-  sampleData    <- getSubjectsForStudy(study_token)
-  subject_data  <- do.call(rbind,lapply(sampleData,unlist ))
-  
-  experiments  <- list()
-  sample_maps  <- list()    
-  
-  molten_list <-  lapply(assay_tokens, function(token){ 
-    assay_name =   sanitize_str( assays[[token]][['name']] )
-    
-    assayFeatures = getFeaturesForAssay(token)
-    assayData     = getMeasurementDataForAssay(token)
-    
-    features_measurements = do.call(cbind, lapply(assayData$measurements, function(x){
-      names(x) <- gsub('[.]','',names(x))
-      unlist(x)
-    } ))
-    
-    if(length(features_measurements) == 0 ){
-      warning(paste0('Empty assay for token ',token,' name ',assay_name))
-      return(NULL)
-    }
-    
-    # map sample ids                                              
-    mapped_sample_list <- lapply( strsplit(rownames(features_measurements),split = '[.]'),function(x){
-      type <- x[1]
-      t1   <- x[2]   
-      t2   <- x[3]
-      subj <- x[4]
-      
-      tvec <- parsePhenoDbTime(t1) + parsePhenoDbTime(t2)
-      tstr <- paste0(paste0(tvec,names(tvec)),collapse = '')
-      
-      c(subj,tstr)
-    })
-    
-    mapped_samples           <- do.call(rbind,mapped_sample_list)
-    colnames(mapped_samples) <- c('name','strtime')
-    mapped_samples           <- as.data.frame(mapped_samples)
-    
-    
-    features_measurements_molten       <- reshape2::melt(data = data.frame(features_measurements,mapped_samples,stringsAsFactors = F),
-                                                         id.var=c('name','strtime'))
-    
-    features_measurements_molten$assay <- assay_name
-    features_measurements_molten       <- features_measurements_molten[,c('assay','name','strtime','variable','value')]
-  })
-  # cleanup list names
-  names(molten_list) <- paste0('token_',gsub('-','_',names(molten_list)))
-  
-  return(molten_list)
-}
-
-mapAssayMeasurement <- function(molten_assay_list,token,variable,group_map){
-  
-  molten_assay_list = paris_molten_assay_list[[token]]
-  
-  row_select        = molten_assay_list$variable == variable & paste(molten_assay_list$strtime) %in% names(group_map)
-  molten_assay_list = molten_assay_list[row_select,]
-  
-  time_mapper <- function(x){
-    group_map[[x]]
-  }
-  
-  molten_assay_list$study_time <- sapply(molten_assay_list$strtime, time_mapper)
-  molten_assay_list
-}                         
-
-
-
-getMultiAssayExp <- function(study_token){
-  require(MultiAssayExperiment)
+                                 
+                                 
+getStudy <- function(study_token){
   require(reshape2)
-  
-  
-  parsePhenoDbTime <- function(x){
-    
-    ptime  <- list(w=0,d=0,h=0,m=0,s=0)
-    
-    val    <- unlist(strsplit(x,split = '[a-z]'))
-    
-    header <- unlist(strsplit(x,split = '[0-9]'))
-    header <- header[!header %in% c('','-')]
-    
-    val        <- as.numeric(val)
-    names(val) <- header
-    val        <- as.list(val)
-    
-    for(dtype in names(val) )
-      ptime[[dtype]] <- val[[dtype]]
-    
-    unlist(ptime)
-    
-  }
-  
-  sanitize_str <- function(str){
-    str                  <- gsub("\\s*\\([^\\)]+\\)","",str)
-    str                  <- gsub("[[:punct:]]", "", str)
-    str                  <- gsub("[[:space:]]", "", str)
-    
-    str
-  }
-  
-  assays       <- getAssaysForStudy(study_token)
-  assay_tokens <- lapply(assays, function(x) x[['token']])# tokens for all assays
-  
-  sampleData    <- getSubjectsForStudy(study_token)
-  subject_data  <- do.call(rbind,lapply(sampleData,unlist ))
-  
-  experiments  <- list()
-  sample_maps  <- list()
-  
-  
-  for(token in assay_tokens){
-    
-    assayFeatures = getFeaturesForAssay(token)
-    assayData     = getMeasurementDataForAssay(token)
-    
-    features_measurements = do.call(cbind, lapply(assayData$measurements, function(x){
-      names(x) <- gsub('[.]','',names(x))
-      unlist(x)
-    } ))
-    
-    if(length(features_measurements) == 0 ){
-      warning(paste0('Empty assay for token ',token,' name ',assay_name))
-      next
+  require(dplyr)
+ 
+  #function to check if a list entry is empty
+  is.NullOb <- function(x) if(!(is.function(x))) is.null(x) | all(sapply(x, is.null)) else FALSE
+
+  ##function to recursively step down into list, removing all empty objects 
+  rmNullObs <- function(x) {
+    if(!(is.function(x))) {
+      x = x[!(sapply(x, is.NullOb))]
+      lapply(x, function(x) if (is.list(x)) rmNullObs(x) else x)
     }
-    
-    # map sample ids                                              
-    mapped_sample_list <- lapply( strsplit(rownames(features_measurements),split = '[.]'),function(x){
-      type <- x[1]
-      t1   <- x[2]   
-      t2   <- x[3]
-      subj <- x[4]
-      
-      tvec <- parsePhenoDbTime(t1) + parsePhenoDbTime(t2)
-      tstr <- paste0(paste0(tvec,names(tvec)),collapse = '')
-      
-      c(subj,tstr)
-    })
-    
-    mapped_samples           <- do.call(rbind,mapped_sample_list)
-    colnames(mapped_samples) <- c('name','strtime')
-    mapped_samples           <- as.data.frame(mapped_samples)
-    
-    
-    features_measurements_molten <- reshape2::melt(data = data.frame(features_measurements,mapped_samples,stringsAsFactors = F),
-                                                   id.var=c('name','strtime'))
-    
-    feature_data                 <- reshape2::dcast(features_measurements_molten, fill = NA,drop = F,
-                                                    value.var = 'value',
-                                                    formula = name+strtime~variable)
-    
-    primary_key               <- paste0(feature_data$name,'_',feature_data$strtime)         
-    colname_key               <- primary_key    
-    
-    rownames(feature_data)    <- primary_key
-    feature_data$name         <- NULL  
-    feature_data$strtime      <- NULL  
-    
-    
-    # add times
-    # add treatment/group information    
-    assay_sample_map      = data.frame(primary = as.character(primary_key),
-                                       colname = as.character(colname_key) )
-    
-    # retrive and sanitize assay name
-    assay_name                  <- assays[[token]][['name']]
-    assay_name                  <- sanitize_str(assay_name)
-    
-    sample_maps[[ assay_name ]] <-  assay_sample_map
-    experiments[[ assay_name ]] <-  t(feature_data)
   }
-  
-  col_data           <- do.call(rbind,lapply(sample_maps,function(x) x))
-  col_data           <- col_data[!duplicated(col_data$primary),]
-  col_data$name      <- unlist(lapply(strsplit(paste(col_data$primary),split = '_'),function(x) x[[1]] ))
-  col_data           <- merge(col_data,subject_data,by='name')
-  rownames(col_data) <- col_data$primary
-  col_data$str_time  <- unlist(lapply(strsplit(paste(col_data$primary),split = '_'),function(x)x[[2]]))
-  col_data$colname   <- NULL
-  
-  multi_sample_map <- listToMap(sample_maps)                                 
-  
-  multi_assay_exp  <- MultiAssayExperiment(experiments = experiments,
-                                           colData   = col_data,
-                                           sampleMap = multi_sample_map)
-  return(multi_assay_exp)
-  
+    
+  ## Get assays for the selected study
+  assays = getAssaysForStudy(studyToken)
+  assayTokens = names(assays)
+  assayNames = sapply(assays, function(x) x$name)             
+             
+  #get subjects in study
+  studySubjects = getSubjectsForStudy(studyToken)
+  studySubjects = lapply(studySubjects, function(x) sapply(x, function(x) ifelse(is.null(x), NA, x)))
+  studySubjects = as.data.frame(do.call(rbind, studySubjects))  
+                                                           
+  #get features in study and subjects in each assay
+  studyFeatures         = list()
+  studySubjectsPerAssay = list()
+                                                           
+  for (i in seq_along(assayTokens)){
+    assayToken                 = assayTokens[i]
+    studyFeatures[[i]]         = getFeaturesForAssay(assayToken)
+    studySubjectsPerAssay[[i]] = getSubjectsForAssay(assayToken)
+  }
+                                                           
+  studyFeaturesWithData       = sapply(studyFeatures, function(x) length(x) > 0)
+  studyFeatures               = rmNullObs(studyFeatures)
+  studyFeaturesLong           = melt(studyFeatures)
+  colnames(studyFeaturesLong) = c("Value", "Property", "Feature", "Assay")
+  studyFeaturesLong$Assay     = assayNames[studyFeaturesWithData][studyFeaturesLong$Assay]
+  studyFeaturesWide           = dcast(data = studyFeaturesLong, formula = Assay + Feature ~ Property, value.var = "Value")
+
+  names(studySubjectsPerAssay) = assayNames
+  studySubjectsPerAssay        = rmNullObs(studySubjectsPerAssay)
+
+  #get all study data
+  studyData = list()
+  for (i in seq_along(assayTokens)){
+    assayToken     = assayTokens[i]
+    studyData[[i]] = getMeasurementDataForAssay(assayToken)$measurements
+  }
+  names(studyData) = assayNames
+
+  #studyData = melt(studyData[sapply(studyData, function(x) x$count > 0)])
+  studyData           = melt(studyData)
+  colnames(studyData) = c("Value", "Subject", "SampleType", "SubjectGroup", "RelTime", "AbsTime", "EventGroup", "Variable", "Assay")
+                                                           
+  return(studyData)                                                         
 }
